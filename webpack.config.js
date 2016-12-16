a86a5f8e10f86892cbda0c3bcd109a65768e067c
loujiahao <loujh@mizlicai.com>
2016-12-16 14:45:29 +0800

    删除不使用的插件

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const OpenBrowserPlugin = require('open-browser-webpack-plugin')
 
 const config = {
-    devtool: 'eval-source-map', //开发环境使用;线上环境请禁用
     entry: {
         app:__dirname + '/src/main.js'
     },
@@ -32,7 +31,6 @@ const config = {
     },
     plugins: [
         new OpenBrowserPlugin({ url: 'http://localhost:8081' }),
-        new webpack.HotModuleReplacementPlugin(),           //热加载插件
         new webpack.NoErrorsPlugin(),                       //允许错误不打断程序
     ]
 };
